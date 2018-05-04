9c7465eedde194338e3fe64da0bd2d22365602ac
Plotogandr <floczar@gmail.com>
2018-05-04 15:01:53 +0200

    First version of my cipher collection

---
+++ src/ciphers/Caesar.java
@@ -0,0 +1,24 @@
+package ciphers;
+
+public class Caesar {
+	
+	static String encrypt(String text, int key) {
+		
+		String encryptedText = "";
+		char charCode;
+		int charCodeShifted;
+		text = text.toLowerCase();
+		text = text.replaceAll("\\s", "");
+		for (int i = 0; i < text.length(); i++) {
+			charCode = text.charAt(i);
+			if (charCode >= 'a' && charCode <= 'z') {
+				charCodeShifted = charCode + key;
+				if (charCodeShifted > 'z')
+					charCodeShifted -= 26;
+				charCode = (char) charCodeShifted;
+			}
+			encryptedText += charCode;
+		}		
+		return encryptedText;
+	}
+}
---
+++ src/ciphers/Vigenere.java
@@ -0,0 +1,30 @@
+package ciphers;
+
+public class Vigenere {
+		
+	static String encrypt(String text, String key) {
+		
+		String encryptedText = "";
+		char charCode;
+		int charCodeShifted;
+		int keypos = 0;
+		text = text.toLowerCase();
+		text = text.replaceAll("\\s", "");
+		for (int i = 0; i < text.length(); i++) {
+			charCode = text.charAt(i);
+			if (charCode >= 'a' && charCode <= 'z') {
+				charCodeShifted = charCode + key.charAt(keypos) - 64;
+				keypos++;
+				if (charCodeShifted > 'z') {
+					charCodeShifted -= 26;
+				}
+				charCode = (char) charCodeShifted;
+				if (keypos == key.length()) {
+					keypos = 0;
+				}
+			}
+			encryptedText += charCode;
+		}
+		return encryptedText;
+	}
+}
