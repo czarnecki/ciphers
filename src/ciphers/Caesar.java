8a27a4b574d5e7c65289098192cd2a2e49ae6ef1
Florian Czarnecki <florian.czarnecki@adesso.de>
2019-01-13 09:06:11 +0100

    Updated formatting

--- src/ciphers/Caesar.java
+++ src/ciphers/Caesar.java
@@ -1,50 +1,50 @@
 package ciphers;
 
 public class Caesar {
-	
-	static String encrypt(String text, int key) {
-		String cipherText = "";
-		char charCode;
-		int charCodeShifted;
-		for (int i = 0; i < text.length(); i++) {
-			charCode = text.charAt(i);
-			if (charCode >= 'A' && charCode <= 'Z') {
-				charCodeShifted = charCode + key;
-				if (charCodeShifted > 'Z')
-					charCodeShifted -= 26;
-				charCode = (char) charCodeShifted;
-			}
-			if (charCode >= 'a' && charCode <= 'z') {
-				charCodeShifted = charCode + key;
-				if (charCodeShifted > 'z')
-					charCodeShifted -= 26;
-				charCode = (char) charCodeShifted;
-			}
-			cipherText += charCode;
-		}
-		return cipherText;
-	}
-	
-	static String decipher(String cipherText, int key) {
-		String originalText = "";
-		char charCode;
-		int charCodeShifted;
-		for (int i = 0; i < cipherText.length(); i++) {
-			charCode = cipherText.charAt(i);
-			if (charCode >= 'A' && charCode <= 'Z') {
-				charCodeShifted = charCode - key;
-				if (charCodeShifted < 'A')
-					charCodeShifted += 26;
-				charCode = (char) charCodeShifted;
-			}
-			if (charCode >= 'a' && charCode <= 'z') {
-				charCodeShifted = charCode - key;
-				if (charCodeShifted < 'a')
-					charCodeShifted += 26;
-				charCode = (char) charCodeShifted;
-			}
-			originalText += charCode;
-		}
-		return originalText;
-	}
+
+    static String encrypt(String text, int key) {
+        String cipherText = "";
+        char charCode;
+        int charCodeShifted;
+        for (int i = 0; i < text.length(); i++) {
+            charCode = text.charAt(i);
+            if (charCode >= 'A' && charCode <= 'Z') {
+                charCodeShifted = charCode + key;
+                if (charCodeShifted > 'Z')
+                    charCodeShifted -= 26;
+                charCode = (char) charCodeShifted;
+            }
+            if (charCode >= 'a' && charCode <= 'z') {
+                charCodeShifted = charCode + key;
+                if (charCodeShifted > 'z')
+                    charCodeShifted -= 26;
+                charCode = (char) charCodeShifted;
+            }
+            cipherText += charCode;
+        }
+        return cipherText;
+    }
+
+    static String decipher(String cipherText, int key) {
+        String originalText = "";
+        char charCode;
+        int charCodeShifted;
+        for (int i = 0; i < cipherText.length(); i++) {
+            charCode = cipherText.charAt(i);
+            if (charCode >= 'A' && charCode <= 'Z') {
+                charCodeShifted = charCode - key;
+                if (charCodeShifted < 'A')
+                    charCodeShifted += 26;
+                charCode = (char) charCodeShifted;
+            }
+            if (charCode >= 'a' && charCode <= 'z') {
+                charCodeShifted = charCode - key;
+                if (charCodeShifted < 'a')
+                    charCodeShifted += 26;
+                charCode = (char) charCodeShifted;
+            }
+            originalText += charCode;
+        }
+        return originalText;
+    }
 }
--- src/ciphers/Ciphers.java
+++ src/ciphers/Ciphers.java
@@ -3,111 +3,111 @@ package ciphers;
 import java.util.Scanner;
 
 public class Ciphers {
-	
-	static Scanner scanInput = new Scanner(System.in);
-	
-	public static void main(String[] args) {
-		int cipherChoice;
-		boolean done = true;
-		System.out.println("  1) Caesar  2) Vigenere\n");
-		do {
-			done = true;
-			cipherChoice = takeChoiceInt("Enter a selection: ");
-			System.out.println();
-			switch (cipherChoice) {
-			case 1:
-				caesarChoice();
-				break;
-			case 2:
-				vigenereChoice();
-				break;
-			default:
-				System.out.println("This option doesn't exist\n");
-				done = false;
-			}
-		} while (!done);
-	}
-	
-	public static void caesarChoice() {
-		boolean done;
-		System.out.println("  1) Cipher  2) Decipher\n");
-		do {
-			done = true;
-			switch (takeChoiceInt("Enter a selection: ")) {
-			case 1:
-				String text;
-				int intKey;
-				System.out.print("Text to cipher: ");
-				text = scanInput.nextLine();
-				intKey = takeChoiceInt("Shift amount: ");
-				System.out.println(Caesar.encrypt(text, intKey));
-				break;
-			case 2:
-				String cipherText;
-				int cipherKey;
-				System.out.print("Ciphertext: ");
-				cipherText = scanInput.nextLine();
-				cipherKey = takeChoiceInt("Used key: ");
-				System.out.println(Caesar.decipher(cipherText, cipherKey));
-				break;
-			default:
-				System.out.print("This option doesn't exist");
-				done = false;
-			}
-		} while (!done);
-	}
-	
-	public static void vigenereChoice() {
-		boolean done;
-		System.out.println("  1) Cipher 2) Decipher");
-		System.out.println();
-		do {
-		done = true;
-			switch (takeChoiceInt("Enter a selection: ")) {
-			case 1:
-				String text;
-				String stringKey;
-				System.out.print("Text to cipher: ");
-				text = scanInput.nextLine();
-				System.out.print("Key as string: ");
-				stringKey = scanInput.nextLine();
-				while(!stringKey.matches("[A-Za-z]+")) {
-					System.out.println(stringKey + " is not a valid key");
-					System.out.print("Key as string: ");
-					stringKey = scanInput.nextLine();
-				}
-				System.out.println(Vigenere.encrypt(text, stringKey));
-				break;
-			case 2:
-				String cipherText;
-				String cipherKey;
-				System.out.print("Ciphertext: ");
-				cipherText = scanInput.nextLine();
-				System.out.print("Key as string: ");
-				cipherKey = scanInput.nextLine();
-				while(!cipherKey.matches("[A-Za-z]+")) {
-					System.out.println(cipherKey + " is not a valid key"); 
-					System.out.print("Key as string: ");
-					cipherKey = scanInput.nextLine();
-				}
-				System.out.println(Vigenere.decipher(cipherText, cipherKey));
-				break;
-			default:
-				System.out.println("This option doesn't exist");
-				done = false;
-			}
-		} while (!done);
-	}
 
-	public static int takeChoiceInt(String response) {
-		int intSelection;
-		System.out.print(response);
-		while (!scanInput.hasNextInt()) {
-			System.out.println(scanInput.nextLine() + " is not a valid input");
-			System.out.print(response);
-		}
-		intSelection = scanInput.nextInt();
-		scanInput.nextLine();
-		return intSelection;
-	}
+    static Scanner scanInput = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int cipherChoice;
+        boolean done = true;
+        System.out.println("  1) Caesar  2) Vigenere\n");
+        do {
+            done = true;
+            cipherChoice = takeChoiceInt("Enter a selection: ");
+            System.out.println();
+            switch (cipherChoice) {
+            case 1:
+                caesarChoice();
+                break;
+            case 2:
+                vigenereChoice();
+                break;
+            default:
+                System.out.println("This option doesn't exist\n");
+                done = false;
+            }
+        } while (!done);
+    }
+
+    public static void caesarChoice() {
+        boolean done;
+        System.out.println("  1) Cipher  2) Decipher\n");
+        do {
+            done = true;
+            switch (takeChoiceInt("Enter a selection: ")) {
+            case 1:
+                String text;
+                int intKey;
+                System.out.print("Text to cipher: ");
+                text = scanInput.nextLine();
+                intKey = takeChoiceInt("Shift amount: ");
+                System.out.println(Caesar.encrypt(text, intKey));
+                break;
+            case 2:
+                String cipherText;
+                int cipherKey;
+                System.out.print("Ciphertext: ");
+                cipherText = scanInput.nextLine();
+                cipherKey = takeChoiceInt("Used key: ");
+                System.out.println(Caesar.decipher(cipherText, cipherKey));
+                break;
+            default:
+                System.out.print("This option doesn't exist");
+                done = false;
+            }
+        } while (!done);
+    }
+
+    public static void vigenereChoice() {
+        boolean done;
+        System.out.println("  1) Cipher 2) Decipher");
+        System.out.println();
+        do {
+            done = true;
+            switch (takeChoiceInt("Enter a selection: ")) {
+            case 1:
+                String text;
+                String stringKey;
+                System.out.print("Text to cipher: ");
+                text = scanInput.nextLine();
+                System.out.print("Key as string: ");
+                stringKey = scanInput.nextLine();
+                while(!stringKey.matches("[A-Za-z]+")) {
+                    System.out.println(stringKey + " is not a valid key");
+                    System.out.print("Key as string: ");
+                    stringKey = scanInput.nextLine();
+                }
+                System.out.println(Vigenere.encrypt(text, stringKey));
+                break;
+            case 2:
+                String cipherText;
+                String cipherKey;
+                System.out.print("Ciphertext: ");
+                cipherText = scanInput.nextLine();
+                System.out.print("Key as string: ");
+                cipherKey = scanInput.nextLine();
+                while(!cipherKey.matches("[A-Za-z]+")) {
+                    System.out.println(cipherKey + " is not a valid key");
+                    System.out.print("Key as string: ");
+                    cipherKey = scanInput.nextLine();
+                }
+                System.out.println(Vigenere.decipher(cipherText, cipherKey));
+                break;
+            default:
+                System.out.println("This option doesn't exist");
+                done = false;
+            }
+        } while (!done);
+    }
+
+    public static int takeChoiceInt(String response) {
+        int intSelection;
+        System.out.print(response);
+        while (!scanInput.hasNextInt()) {
+            System.out.println(scanInput.nextLine() + " is not a valid input");
+            System.out.print(response);
+        }
+        intSelection = scanInput.nextInt();
+        scanInput.nextLine();
+        return intSelection;
+    }
 }
